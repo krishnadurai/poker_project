f0d566ba85b7b890bed7a502d7200f8993abd91d
ashwani dausodia <ashwani27493@gmail.com>
2014-03-19 20:27:54 +0530

    amounts handled in round 1

--- dealer.py
+++ dealer.py
@@ -9,47 +9,64 @@ import threading
 import thread 
 
 # Variable Initialisation
-NO_OF_PLAYERS = 3
+NO_OF_PLAYERS = 5
+small_blind_amt = 1
 
 INITIAL_MONEY = 1000
 
 MINIMUM_RAISE = 1
 
-small_blind=random.randint(0, NO_OF_PLAYERS-1);
+small_blind=random.randint(0, NO_OF_PLAYERS - 1);
 
 
-# Money in players hand
-players_money = []
+# Received data from thread
+class recvd_data_from_thread:
+    curr_data_recvd = ''
+    def __init__(self):
+        pass
+
+thread_recvd_data = recvd_data_from_thread()
+
+# Players involved currently
+live_players = []
 for i in range(0, NO_OF_PLAYERS):
-    players_money.append(INITIAL_MONEY)
+    live_players.append(1)
 
 
-# Players involved in currently
-live_players = ""
-for i in range(0, NO_OF_PLAYERS):
-    live_players += "1"
-
-
-NO_OF_POTS = 1
+no_of_pots= 1
 
 # players in the current pot
-pot_money = [0] * 8
+pot_money = []
+pot_money.append(small_blind_amt * 3)
 
 # Will be chaged when pot life ends
 pot_players = []
-current_round = 0
+current_round = 1
 
 
 # Player investment in round
 pot_investment = [0] * NO_OF_PLAYERS
-pot_investment[small_blind] = 1
+pot_investment[small_blind] = small_blind_amt
 big_blind = (small_blind + 1) % NO_OF_PLAYERS
-pot_investment[big_blind] = 2
+pot_investment[big_blind] = small_blind_amt * 2
 current_player = (big_blind + 1 ) % NO_OF_PLAYERS
 #current_player = 2
 # Player who raised last
 last_raised = big_blind
 last_raised_amt = pot_investment[big_blind]
+last_raised_by = 0
+
+# Money in players hand
+players_money = []
+for i in range(0, NO_OF_PLAYERS):
+    players_money.append(INITIAL_MONEY)
+    players_money[i] -= pot_investment[i]
+
+
+print "--------------------PLAYERS MONEY-----------------------"
+print players_money
+
+
 
 # data
 SUITS = ('c', 'd', 'h', 's')
@@ -57,6 +74,8 @@ RANKS = ('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A')
 
 #DECK stores all cards
 DECK = tuple(''.join(card) for card in itertools.product(RANKS,SUITS))
+
+# Generate Lookups
 SUIT_LOOKUP = dict(zip(SUITS, range(4)))
 RANK_LOOKUP = dict(zip(RANKS, range(13)))
 ORDER_LOOKUP={}
@@ -76,6 +95,7 @@ def recv_data():
             if data != '':
                 print 'Data received in recv data is ' + data
                 done = 1
+                thread_recvd_data.curr_data_recvd =  data
         except:
             print 'wtf in recv_data'
 
@@ -93,11 +113,43 @@ def send_them_all(data):
             sock.send(data)
         except:
             print 'wtf in send_data'
+
+# Get Number of live players
+def get_no_of_live_players():
+    sum = 0
+    for i in range(0,len(live_players)):
+        sum = sum + live_players[i]
+    return sum
+# Get Next player
+
+def get_next_player(curr):
+    i = (int(curr) + 1) % NO_OF_PLAYERS
+    #print ' i is : ' + str(i)
+    while(i != curr):
+        if (live_players[i] == 1):
+            return i
+        i = (i + 1) % NO_OF_PLAYERS
+    return curr
+# get playerlist
+def get_player_list(round_num):
+    player_list = []
+    if round_num == 1:
+        player_num = (small_blind + 2) % NO_OF_PLAYERS
+    else:
+        player_num = small_blind
+    for i in range(0, NO_OF_PLAYERS):
+        if live_players[player_num] == 1:
+            player_list.append(player_num)
+        player_num = (player_num + 1) % NO_OF_PLAYERS 
+    return player_list
+
+
 # Main
 send_sock=[]
 recv_conn = []
 def main():
     global current_player
+    global last_raised_amt, last_raised_by
     s = socket(AF_INET, SOCK_STREAM)
     s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     R_HOST = ''
@@ -119,7 +171,7 @@ def main():
         if(len(ip_array) == NO_OF_PLAYERS):
             break
 
-# Check conn
+    # Check conn
     i = 0
     S_PORT = 11716
     while(i < NO_OF_PLAYERS):
@@ -132,6 +184,7 @@ def main():
         send_sock[i].send(data)
         i += 1
 
+    # Round Starts now
     while(True):
         time.sleep(1)
         i = 0
@@ -145,7 +198,7 @@ def main():
             print 'initial data sent to '
             print S_HOST
 
-#Sending card
+        #Sending card
         time.sleep(1)
         i = 0
         print ' Sending cards to players'
@@ -156,22 +209,128 @@ def main():
             i += 1
 
         i = 0
+
+        # Sending Public Data
         print 'sending data '
         time.sleep(1)
         while(i < NO_OF_PLAYERS):
-            data = 'req=data:' + 'nop=' + str(NO_OF_PLAYERS) + ';live=' + live_players + ';yourPos=' + str(i) + ";sb=" + str(small_blind) + ";current_player=" + str(current_player) + ";NO_OF_POTS=" + str(NO_OF_POTS) + ";players_money=" + str(players_money) + ";pot_investment=" +str(pot_investment) + ";last_raised_amt=" + str(last_raised_amt)
+            data = 'req=data:' + 'nop=' + str(NO_OF_PLAYERS) + ';live=' + str(live_players) + ';yourPos=' + str(i) + ";sb=" + str(small_blind) + ";current_player=" + str(current_player) + ";NO_OF_POTS=" + str(no_of_pots) + ";players_money=" + str(players_money) + ";pot_investment=" +str(pot_investment) + ";last_raised_amt=" + str(last_raised_amt)
             thread.start_new_thread(send_data, (data,i,))
             i += 1
-        while(current_player != last_raised): 
-            print 'current player is ' + str(current_player)
+
+        # Game starts now
+        # ALL play
+        time.sleep(1)
+        print ' GAME STARTS NOW>>>>>>>>>'
+        somebody_raised = False
+        p_list = get_player_list(current_round)
+        print p_list
+        for player in p_list:
             time.sleep(1)
+            if get_no_of_live_players() <= 1:
+                print 'Only one Player left'
+                break
+
+            current_player = player
+            send_them_all('req=data:current_player=' + str(current_player))
+            print ' in all play current_player = ' + str(current_player)
+
             t = threading.Thread(target = recv_data , args = ())
             t.start()
             t.join()
-            current_player = (current_player + 1) % NO_OF_PLAYERS
-            if current_player == last_raised:
+            print 'curr_data_recvd after join is ' + thread_recvd_data.curr_data_recvd
+            req_type = thread_recvd_data.curr_data_recvd.partition(':')[0].partition('=')[2]
+            if req_type == 'raiseTO':
+                print str(current_player) + '  raised'
+                somebody_raised = True
+                last_raised = current_player
+                last_raised_by = int(thread_recvd_data.curr_data_recvd.partition(':')[2].partition('=')[2]) - last_raised_amt
+                last_raised_amt += last_raised_by
+                players_money[current_player] -= (last_raised_amt - pot_investment[current_player])
+                pot_investment[current_player] = last_raised_amt
+                current_player = get_next_player(current_player)
+                time.sleep(1)
+                send_them_all('req=data:current_player=' + str(current_player) + ';last_raised_amt=' + str(last_raised_amt) + ';last_raised_by=' + str(last_raised_by) + ';players_money=' + str(players_money) + ';pot_investment=' + str(pot_investment)) 
                 break
-            send_them_all('req=data:current_player=' + str(current_player))
+
+            elif req_type == 'fold':
+                print str(current_player) + ' folded'
+                live_players[current_player] = 0
+                send_them_all('req=data:live=' + str(live_players))
+
+            elif req_type == 'call':
+                print str(current_player) + '  called'
+                if (last_raised_amt == int(thread_recvd_data.curr_data_recvd.partition(':')[2].partition('=')[2])):
+                    if players_money[current_player] >= (last_raised_amt - players_money[current_player]):
+                        players_money[current_player] -= (last_raised_amt - pot_investment[current_player])
+                        pot_investment[current_player] = last_raised_amt
+                        send_them_all('req=data:players_money='+str(players_money) + ';pot_investment=' + str(pot_investment))
+                    else:
+                        print 'player ',current_player,' has ',players_money[current_player],' money, req is ',last_raised_amt
+                else:
+                    pass
+                    # Split pot
+
+
+        print 'somebody_raised is ' + str(somebody_raised)
+        # If somebody raised
+        if somebody_raised:
+            print 'current_player an last_raised are ' + str(current_player) + '  :::::  ' +  str(last_raised)
+
+            while(current_player != last_raised): 
+                if get_no_of_live_players() <= 1:
+                    print 'Only one Player left'
+                    break
+
+                print ' in raise play current_player = ' + str(current_player)
+
+                time.sleep(1)
+                t = threading.Thread(target = recv_data , args = ())
+                t.start()
+                t.join()
+
+                req_type = thread_recvd_data.curr_data_recvd.partition(':')[0].partition('=')[2]
+                if req_type == 'raiseTO':
+                    last_raised = current_player
+                    last_raised_by = int(thread_recvd_data.curr_data_recvd.partition(':')[2].partition('=')[2]) - last_raised_amt
+                    last_raised_amt += last_raised_by
+                    players_money[current_player] -= (last_raised_amt - pot_investment[current_player])
+                    pot_investment[current_player] = last_raised_amt
+                    time.sleep(1)
+                    send_them_all('req=data:current_player=' + str(current_player) + ';last_raised_amt=' + str(last_raised_amt) + ';last_raised_by=' + str(last_raised_by) + ';players_money=' + str(players_money) + ';pot_investment=' + str(pot_investment)) 
+
+                elif req_type == 'fold':
+                    print str(current_player) + '  folded'
+                    live_players[current_player] = 0
+                    send_them_all('req=data:live=' + str(live_players))
+
+                elif req_type == 'call':
+                    print str(current_player) + '  called'
+                    if (last_raised_amt == int(thread_recvd_data.curr_data_recvd.partition(':')[2].partition('=')[2])):
+                        if players_money[current_player] >= last_raised_amt:
+                            players_money[current_player] -= (last_raised_amt - pot_investment[current_player])
+                            pot_investment[current_player] = last_raised_amt
+                            send_them_all('req=data:players_money='+str(players_money) + ';pot_investment=' + str(pot_investment))
+                        else:
+                            print 'player ',current_player,' has ',players_money[current_player],' money, req is ',last_raised_amt
+                    else:
+                        pass
+                        # Split pot
+
+                current_player = get_next_player(current_player)
+                # Round ends Here
+                if current_player == last_raised:
+                    break
+                time.sleep(1)
+                print '---------------END OF LOOP--------------------'
+                print 'current players is  => ',current_player 
+                print 'last_raised is  => ',last_raised
+                print 'players_money is => ',players_money
+                print 'pot_investment is => ',pot_investment
+
+                send_them_all('req=data:current_player=' + str(current_player))
+
         sys.exit()
+
 if __name__  ==  '__main__':
     main()
--- ui.py
+++ ui.py
@@ -11,7 +11,7 @@ import platform
 from pgu import gui
 
 #Socket to receive data
-DEALER_SERVER = '192.168.117.4'
+DEALER_SERVER = '192.168.117.2'
 R_PORT = 11716
 R_HOST = ''
 sock_recv_data = socket(AF_INET, SOCK_STREAM)
@@ -63,8 +63,10 @@ def handle_data():
                 paramlist = temp.split(';')
                 for param in paramlist:
                     if param.partition('=')[0] == 'live':
+                        temp = param.partition('=')[2]
+                        temp = temp.strip('[').strip(']').split(',')
                         for i in range(0, poker_data.NO_OF_PLAYERS):
-                            poker_data.ingame[i] = int(param.partition('=')[2])
+                            poker_data.ingame[i] = int(temp[i])
                     elif param.partition('=')[0] == 'current_player':
                         poker_data.current_player = int(param.partition('=')[2])
                     elif param.partition('=')[0] == 'NO_OF_POTS':
@@ -81,6 +83,12 @@ def handle_data():
                         temp = temp.strip('[').strip(']').split(',')
                         for i in range(len(temp)):
                             poker_data.money_in_pot[i] = int(temp[i])
+                    elif param.partition('=')[0] == 'last_raised_by':
+                        poker_data.last_raised_by = int(param.partition('=')[2])
+                    elif param.partition('=')[0] == 'small_blind_amt':
+                        poker_data.small_blind_amt = int(param.partition('=')[2])
+            poker_data.bet_amount = max(poker_data.last_raised_amt + poker_data.last_raised_by, poker_data.small_blind_amt*2)
+
         else:
             time.sleep(1)
 
@@ -124,22 +132,24 @@ try:
     thread.start_new_thread(handle_data, ())
 except:
     print'unable to start handle_data thread'
-'''
+
 try:
     sock_send_data.connect((S_HOST, S_PORT))
     sock_send_data.send('req')
 except:
     print 'connect error'
-'''
+
 
 class Message:
-    mypos = 8
+    mypos = 0
     small_blind = 5
+    small_blind_amt = 0
     NO_OF_PLAYERS = 0
     ingame = [0, 0, 0, 0, 0, 0, 0, 0]
     money_in_pot = [0, 0, 0, 0, 0, 0, 0, 0]
     remaining_money = [1500, 1500, 1500, 1500, 1500, 1500, 1500, 1500]
     last_raised_amt = 0
+    last_raised_by = 0
     current_player = 0
     bet_amount = 0
     player_cards1 = ["images/front", "images/front", "images/front",
@@ -182,23 +192,22 @@ class bet_bar(gui.Table):
         
         self.tr()
         self.td(gui.Label("Amount: ",color=fg),align=1)
-        e = gui.HSlider(50,0,1000,size=10,width=500,height=16,name='amount')
+        e = gui.HSlider(0,0,1000,size=10,width=500,height=16,name='amount')
         e.connect(gui.CHANGE,self.adjust,(0,e))
         self.td(e)
     def adjust(self,value):
         (num, slider) = value
         self.value[num] = slider.value
-        poker_data.bet_amount = int(slider.value)
-        print poker_data.bet_amount
-
-form = gui.Form()
-
+        amt_min = max(poker_data.last_raised_amt + poker_data.last_raised_by, poker_data.small_blind_amt*2)
+        amt_max = poker_data.remaining_money[poker_data.mypos]
+        poker_data.bet_amount = int(slider.value)/1000. * (amt_max-amt_min) + amt_min
+        poker_data.bet_amount = int(poker_data.bet_amount)
+        
 app = gui.App()
 betBar = bet_bar()
 
 c = gui.Container(align=-1,valign=-1)
 c.add(betBar,50,460)
-
 
 pygame.init()
 app.init(c)
@@ -210,8 +219,9 @@ icon = pygame.image.load("images/icon.png").convert()
 button_fold = pygbutton.PygButton((50, 420, 100, 30), 'FOLD')
 button_call = pygbutton.PygButton((280, 420, 150, 30), 'CALL / CHECK')
 button_raise = pygbutton.PygButton((550, 420, 150, 30), 'BET / RAISE')
-button_turn = pygbutton.PygButton((50, 460, 100, 30), 'Your Turn')
-
+button_turn = pygbutton.PygButton((50, 20, 100, 30), 'Your Turn')
+current_player_image = pygame.image.load("images/current_player").convert()
+small_blind_image = pygame.image.load("images/sb.png").convert()
 #button = pygame.image.load("images/button").convert()
 pygame.display.set_icon(icon)
 back = pygame.image.load(bg).convert()
@@ -221,7 +231,6 @@ clock = pygame.time.Clock()
 
 while True:
     font = pygame.font.Font(None, 24)
-    small_blind_image = pygame.image.load("images/sb.png").convert()
     hc_images = [pygame.image.load(poker_data.hand_cards[0]).convert(),
                  pygame.image.load(poker_data.hand_cards[1]).convert()]
     cc_image = []
@@ -235,7 +244,7 @@ while True:
                            .convert())
         card2_image.append(pygame.image.load(poker_data.player_cards2[i])
                            .convert())
-    clock.tick(15)
+    clock.tick(10)
     for event in pygame.event.get():
         fold_events = button_fold.handleEvent(event)
         call_events = button_call.handleEvent(event)
@@ -247,16 +256,18 @@ while True:
             print 'Fold button clicked'
             poker_data.hand_cards[0] = 'images/front'
             poker_data.hand_cards[1] = 'images/front'
+            thread.start_new_thread(send_data, ('req=fold:',))
         elif 'click' in call_events:
             print 'CALL button clicked'
             if poker_data.current_player == poker_data.mypos:
                 print 'attempting to send data'
-                thread.start_new_thread(send_data, (poker_data.last_raised_amt,))
+                thread.start_new_thread(send_data, ('req=call:amt='+str(min(poker_data.last_raised_amt, poker_data.remaining_money[poker_data.mypos])),))
         elif 'click' in raise_events:
             print 'RAISE button clicked'
             if poker_data.current_player == poker_data.mypos:
                 print 'attempting to send data'
-                thread.start_new_thread(send_data, (poker_data.last_raised_amt+1,))
+                slider_value = str(poker_data.bet_amount)
+                thread.start_new_thread(send_data, ('req=raiseTO:amt='+str(poker_data.bet_amount),))
         elif event.type is KEYDOWN and event.key == K_ESCAPE: 
             pass
         else:
@@ -310,8 +321,15 @@ while True:
     screen.blit(check, (535, 465))
     '''
     button_turn.visible = False
+    button_call.visible = False
+    button_raise.visible = False
+    button_fold.visible = False
     if poker_data.mypos == poker_data.current_player:
         button_turn.visible = True
+        button_call.visible = True
+        if poker_data.last_raised_amt < poker_data.remaining_money[poker_data.mypos]:
+            button_raise.visible = True
+        button_fold.visible = True
     button_call.draw(screen)
     button_raise.draw(screen)
     button_fold.draw(screen)
@@ -319,8 +337,14 @@ while True:
     sb = 8 + poker_data.small_blind - poker_data.mypos
     sb %= 8
     screen.blit(small_blind_image, (pos[sb][0]- 30, pos[sb][1]))
-    bet_text = font.render('$ '+str(poker_data.bet_amount), 1, (255, 255, 255))
-    screen.blit(bet_text, (700, 480))
+    cp = 8 + poker_data.current_player - poker_data.mypos
+    cp %= 8
+    screen.blit(current_player_image, (pos[cp][0]-16, pos[cp][1] + 20))
+    if poker_data.mypos == poker_data.current_player:
+        bet_text = font.render('$ '+str(poker_data.bet_amount), 1, (255, 255, 255))
+        screen.blit(bet_text, (700, 480))
+        call_text = font.render('$ '+str(min(poker_data.last_raised_amt, poker_data.remaining_money[poker_data.mypos])), 1, (255, 255, 255))
+        screen.blit(call_text, (450, 425))
     app.paint()
     pygame.display.update()
 
